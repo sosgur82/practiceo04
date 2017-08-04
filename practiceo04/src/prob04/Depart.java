e33f67862c662b1dc99e3c2073f420a8494c377b
BIT <BIT@DESKTOP-K00OCLP>
2017-08-04 17:49:44 +0900

    sourse change

--- practiceo04/src/prob04/Depart.java
+++ practiceo04/src/prob04/Depart.java
@@ -2,14 +2,14 @@ package prob04;
 
 public class Depart extends Employee {
 	private String department;
-	public Depart(String name, int salary, String dept) {
+	public Depart(String name, int salary) {
 		super(name,salary);
 	}
 	public Depart() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
-	public Depart(String name, int salary) {
+	public Depart(String name, int salary, String dept) {
 		super(name, salary);
 		// TODO Auto-generated constructor stub
 		this.department = dept;
